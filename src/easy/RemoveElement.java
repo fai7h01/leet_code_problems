2467b32836aaf61dc4362e68d65c4d3a33b55b6e
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-19 16:04:18 +0400

    RemoveElement

--- src/easy/RemoveElement.java
+++ src/easy/RemoveElement.java
@@ -7,6 +7,7 @@ public class RemoveElement {
     public static void main(String[] args) {
 
         System.out.println(removeElement(new int[]{3, 2, 2, 3}, 3));
+        System.out.println(removeElementUsingTemp(new int[]{3, 2, 2, 3}, 3));
 
     }
 
@@ -23,6 +24,22 @@ public class RemoveElement {
                 index++;
             }
         }
+        return index;
+    }
+
+    private static int removeElementUsingTemp(int nums[], int val){
+
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val){
+                int temp = nums[index];
+                nums[index] = nums[i];
+                nums[i] = temp;
+                index++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
         return index;
     }
 
