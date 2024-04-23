c817eaf98c5757c55c896f6e28545ce9708b8b92
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-23 15:06:13 +0400

    ValidPalindrome

---
+++ src/easy/ValidPalindrome.java
@@ -0,0 +1,35 @@
+package easy;
+
+public class ValidPalindrome {
+
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+
+    }
+
+
+    public static boolean isPalindrome(String s){
+
+        if (s.isEmpty()) return true;
+
+        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+
+        int start = 0;
+        int end = s.length() - 1;
+
+        while(start <= end){
+            if (s.charAt(start) == s.charAt(end)){
+                start++;
+                end--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+}
+
