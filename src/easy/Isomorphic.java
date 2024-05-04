ca8c83dec6e2f1b802966890dc84a3032119ff21
fai7h01 <l.varsimashvil10@gmail.com>
2024-05-04 16:43:46 +0400

    IsIsomorphic

--- src/easy/Isomorphic.java
+++ src/easy/Isomorphic.java
@@ -3,8 +3,7 @@ package easy;
 public class Isomorphic {
     public static void main(String[] args) {
 
-        System.out.println(isIsomorphic("egg", "add"));
-
+        System.out.println(isIsomorphic("apple", "rttoq"));
 
     }
 
@@ -18,14 +17,11 @@ public class Isomorphic {
         if (len != t.length()) return false;
 
         for (int i = 0; i < len; i++) {
-            if (indexS[s.charAt(i)] != indexT[t.charAt(i)]){
-                return false;
-            }
+            if (indexS[s.charAt(i)] != indexT[t.charAt(i)]) return false;
 
             indexS[s.charAt(i)] = i + 1;
             indexT[t.charAt(i)] = i + 1;
         }
-
         return true;
     }
 
