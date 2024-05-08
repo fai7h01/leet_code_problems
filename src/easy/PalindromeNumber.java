6e5e91938089ba44e0c8a7da328e7fd89d6a1117
fai7h01 <l.varsimashvil10@gmail.com>
2024-05-08 16:10:17 +0400

    algo

---
+++ src/easy/PalindromeNumber.java
@@ -0,0 +1,31 @@
+package easy;
+
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome(121));
+    }
+
+    static boolean isPalindrome(int x){
+
+       if (x < 0) return false;
+
+       int reversed = 0;
+       int temp = x;
+
+       while (temp != 0){
+           int digit = temp % 10;
+           reversed = reversed * 10 + digit;
+           temp /= 10;
+       }
+       return reversed == x;
+
+
+
+
+
+    }
+
+
+}
