c9e5a41d4be184afcb5542bf97eb528df23c24d4
fai7h01 <l.varsimashvil10@gmail.com>
2024-05-13 16:02:15 +0400

    remove duplicates 2

---
+++ src/medium/RemoveDuplicate2.java
@@ -0,0 +1,28 @@
+package medium;
+
+public class RemoveDuplicate2 {
+    public static void main(String[] args) {
+
+    }
+
+    static int removeDuplicates2(int[] nums){
+
+        int count = 1;
+        int index = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]){
+                count++;
+            }else{
+                count = 1;
+            }
+            if (count <= 2){
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+
+    }
+
+
+}
