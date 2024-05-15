3a3bab4d2dc114cc0e9ff8504e6c75be5dc914b6
fai7h01 <l.varsimashvil10@gmail.com>
2024-05-15 15:26:32 +0400

    RotateArray

--- src/medium/RotateArray.java
+++ src/medium/RotateArray.java
@@ -7,11 +7,9 @@ public class RotateArray {
     private static void rotate(int[] nums, int k){
 
         k %= nums.length;
-
-        reverseArray(nums, 0, nums.length-1);
-        reverseArray(nums, 0, k-1);
-        reverseArray(nums, k, nums.length-1);
-        System.out.println(Arrays.toString(nums));
+        reverseArray(nums, 0, nums.length - 1);
+        reverseArray(nums, 0, k -1);
+        reverseArray(nums, k, nums.length - 1);
 
     }
 
