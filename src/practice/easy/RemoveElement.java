bd61da2125480d521cb6c9dc297ef663b447b849
fai7h01 <l.varsimashvil10@gmail.com>
2024-05-31 22:28:08 +0400

    remove element

---
+++ src/practice/easy/RemoveElement.java
@@ -0,0 +1,21 @@
+package practice.easy;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+
+    }
+
+    static int removeElement(int[] nums, int val){
+
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val){
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+
+    }
+
+}
