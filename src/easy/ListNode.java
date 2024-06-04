0b92db12cf9b30b5f63c84bf796ea5149d327759
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-04 15:19:01 +0400

    remove nth element from LL

---
+++ src/easy/ListNode.java
@@ -0,0 +1,20 @@
+package easy;
+
+public class ListNode {
+
+    int val;
+    easy.ListNode next;
+
+    public ListNode() {
+    }
+
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, easy.ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/easy/RemoveNthElementFromLL.java
@@ -0,0 +1,36 @@
+package easy;
+
+public class RemoveNthElementFromLL {
+
+    public ListNode removeNthElement(ListNode head, int n){
+
+        ListNode p1 = head;
+        ListNode p2 = head;
+        ListNode prev = null;
+        ListNode tail = null;
+
+        for (int i = 0; i < n - 1; i++) {
+            p2 = p2.next;
+        }
+        while(p2.next != null){
+            prev = p1;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        tail = p2;
+
+        if (p1 == head){
+            head = p1.next;
+            p1.next = null;
+        } else if (p1 == tail) {
+            tail = prev;
+            prev.next = null;
+        }else{
+            prev.next = p1.next;
+            p1.next = null;
+        }
+        return head;
+
+    }
+
+}
