b7f068e45147dc43ea59b11ced37f4dbbff89a2b
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-12 23:59:47 +0400

    test

--- src/test/ReverseWordsInString.java
+++ src/test/ReverseWordsInString.java
@@ -1,13 +1,11 @@
 package test;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class ReverseWordsInString {
     public static void main(String[] args) {
 
         String str = "the    sky is blue";
         System.out.println(reverseWords(str));
+        System.out.println(reverseWordsWithoutRegex(str));
 
     }
 
@@ -19,6 +17,17 @@ public class ReverseWordsInString {
             result.append(array[i]).append(" ");
         }
         return result.toString().trim();
+    }
+
+    public static String reverseWordsWithoutRegex(String str){
+        String[] array = str.trim().split(" ");
+        StringBuilder res = new StringBuilder();
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (!array[i].isEmpty()){
+                res.append(array[i]).append(" ");
+            }
+        }
+        return res.toString().trim();
     }
 
 }
