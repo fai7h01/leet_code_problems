450c125aad02b3f303c8568a7cad1f063324f755
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-20 14:00:35 +0400

    test

---
+++ src/easy/ConcatenationOfArray.java
@@ -0,0 +1,29 @@
+package easy;
+
+import java.util.Arrays;
+
+public class ConcatenationOfArray {
+    public static void main(String[] args) {
+
+        int[] nums = {1,2,1}; // 1 2 1 1 2 1
+        System.out.println(Arrays.toString(getConcatenation(nums)));
+
+    }
+
+    static int[] getConcatenation(int[] nums){
+
+        int[] ans = new int[nums.length * 2];
+
+        int j = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[j++] = nums[i];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[j++] = nums[i];
+        }
+
+        return ans;
+    }
+}
