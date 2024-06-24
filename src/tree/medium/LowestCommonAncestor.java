b91931ac1237fddf9eb8083e732a01c904c47d80
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-24 21:23:27 +0400

    tree problems

---
+++ src/tree/medium/LowestCommonAncestor.java
@@ -0,0 +1,20 @@
+package tree.medium;
+
+import tree.TreeNode;
+
+public class LowestCommonAncestor {
+
+    static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null || root == p || root == q) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) return root;
+
+        return left == null ? right : left;
+
+    }
+
+}
